cd514ea3a878cfc6bbdfafd33220b0bbf46da1f7
luizphelipes <luizphelipes.f@gmail.com>
2025-04-09 20:20:25 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import re
 import logging
 import requests
+from services.instagram_pool import get_instagram_pool
 
 # Configuração básica do logger
 logging.basicConfig(
@@ -21,24 +22,15 @@ logger = logging.getLogger(__name__)  # Para usar com from utils import logger
 
 
 
+
 def check_profile_privacy(username):
     """
-    Verifica se o perfil do Instagram é público ou privado usando a API externa.
+    Verifica se o perfil do Instagram é público ou privado.
+    Utiliza o pool de contas do Instagram.
     """
-    url = f"https://instagram-looter2.p.rapidapi.com/web-profile?username={username}"
-    headers = {
-        "X-Rapidapi-Key": "29def6eec7msh8d994ba439bbd1ap153df5jsn3924152b9568",
-        "X-Rapidapi-Host": "instagram-looter2.p.rapidapi.com"
-    }
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Levanta exceção para status != 200
-        data = response.json()
-        # A API retorna 'is_private' como booleano
-        return "private" if data.get("is_private") else "public"
-    except Exception as e:
-        logger.error(f"Erro ao verificar perfil {username}: {str(e)}")
-        return "error"
+    pool = get_instagram_pool()
+    return pool.check_profile_privacy(username)
+
 
 #Sanitizar username conforme a Yampi 
 def sanitize_customization(customization):
