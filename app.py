03a21bf52ec0acfbbd2173efd6dba7d5f764abfa
luizphelipes <luizphelipes.f@gmail.com>
2025-04-09 18:35:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from database import engine
 from models.base import Base
 from services.scheduler import start_scheduler
 from routes import webhook_bp, payments_bp  # Importe todos os blueprints
-
+from routes.instagram import instagram_bp
 
 
 app = Flask(__name__)
@@ -15,6 +15,7 @@ app = Flask(__name__)
 # Registrar blueprints
 app.register_blueprint(webhook_bp, url_prefix='/api')
 app.register_blueprint(payments_bp, url_prefix='/api')
+app.register_blueprint(instagram_bp, url_prefix='/api')
 # Registre outros blueprints conforme necessário
 
 # Iniciar agendador
