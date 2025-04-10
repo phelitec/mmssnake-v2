1f9d41655c9348535d1e1b514380376c85f26215
luizphelipes <luizphelipes.f@gmail.com>
2025-04-10 00:42:43 -0300

    Update scheduler.py

--- services/scheduler.py
+++ services/scheduler.py
@@ -200,18 +200,11 @@ def update_delivered_orders():
 
 
 def run_scheduled_task():
-    schedule.every(2).minutes.do(check_pending_profiles)
-    schedule.every(1).minutes.do(process_pending_payments)
-    schedule.every().day.at("22:56").do(update_delivered_orders)  # Nova tarefa às 18:00
+    schedule.every(10).minutes.do(check_pending_profiles)
+    schedule.every(5).minutes.do(process_pending_payments)
+    schedule.every().day.at("19:00").do(update_delivered_orders)  # Nova tarefa às 19:00
     logging.info("Agendador configurado para rodar tarefas periódicas.")
-    while True:
-        try:
-            schedule.run_pending()
-            logging.info("Agendador rodando...")
-            time.sleep(30)
-        except Exception as e:
-            logging.error(f"Erro no loop do agendador: {str(e)}")
-            time.sleep(60)
+    
 
 def start_scheduler():
     scheduler_thread = threading.Thread(target=run_scheduled_task)
